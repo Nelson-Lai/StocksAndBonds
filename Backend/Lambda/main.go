b536342032708a5b859dcc28bc019013ebb63c19
Nelson Lai <nelsonl93@gmail.com>
2020-02-19 00:54:24 -0800

    barebones at 1am lol

--- Backend/Lambda/main.go
+++ Backend/Lambda/main.go
@@ -1,8 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
 
 func main() {
 	// Theres nothing here yet!
 	fmt.Println("lol")
+	lambda.Start(Handler)
+}
+
+// Handler handles requests from API Gateway stop bitching at me
+func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	// TODO HANDLER
+	return events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Body:       "Kraz sux",
+	}, nil
 }
